6c3c09585c2a75ccedafdfe734f18bd5f537375c
unknown <frayosorio@gmail.com>
2026-02-12 10:01:30 -0500

    Se agregan los primeros elementos de la Interfaz Grafica

--- src/FrmDistribucionFrecuencias.java
+++ src/FrmDistribucionFrecuencias.java
@@ -1,4 +1,9 @@
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 public class FrmDistribucionFrecuencias extends JFrame {
 
@@ -7,7 +12,38 @@ public class FrmDistribucionFrecuencias extends JFrame {
         setTitle("Distribución de frecuencias");
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
+        setLayout(null);
+
+        JLabel lblPregunta = new JLabel("Pregunta:");
+        lblPregunta.setBounds(10, 10, 100, 20);
+        add(lblPregunta);
+
+        JTextArea txtPregunta = new JTextArea(
+                "¿Cómo considera la calidad de la señal de internet que entra al barrio?");
+        txtPregunta.setBounds(120, 10, 300, 50);
+        txtPregunta.setEditable(false);
+        txtPregunta.setLineWrap(true);
+        add(txtPregunta);
+
+        JLabel lblRespuesta = new JLabel("Respuesta:");
+        lblRespuesta.setBounds(10, 65, 100, 20);
+        add(lblRespuesta);
+
+        JComboBox cmbRespuesta = new JComboBox();
+        cmbRespuesta.setBounds(120, 65, 100, 25);
+        add(cmbRespuesta);
+
+        String[] respuestas = { "Excelente", "Buena", "Regular", "Mala" };
+        cmbRespuesta.setModel(new DefaultComboBoxModel(respuestas));
+
+        JButton btnAgregar = new JButton(">>");
+        btnAgregar.setBounds(10, 95, 100, 25);
+        add(btnAgregar);
+
+        JButton btnQuitar = new JButton("<<");
+        btnQuitar.setBounds(10, 125, 100, 25);
+        add(btnQuitar);
 
     }
 }
