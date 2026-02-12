491fcb95f988ac6205ad0a96a8904e47f5d9661a
unknown <frayosorio@gmail.com>
2026-02-12 09:23:58 -0500

    Se crea la interfaz grafica de usuario

---
+++ src/FrmDistribucionFrecuencias.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class FrmDistribucionFrecuencias extends JFrame {
+
+    // metodo constructor
+    public FrmDistribucionFrecuencias() {
+        setTitle("Distribución de frecuencias");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+    }
+}
